c61d2df78aefccb556de09d37dcc32b7de2f7128
ap-wi <ap-wi@t-online.de>
2016-10-11 12:13:33 +0200

    second commit

---
+++ plot1_r.R
@@ -0,0 +1,28 @@
+## --------------------------------------------------
+## R Coding 
+## Plot 1 Histogramm
+## File "exdata_data_household_power_consumption.zip"
+## 10.10.2016 A.Paul
+## --------------------------------------------------
+
+setwd("C:/Users/Paul/Desktop/coursera")
+
+## File Import
+rawdata <- read.table("household_power_consumption.txt",header=TRUE,sep=";",dec=".")
+
+helpdate <- substring(mydata$Date,1,10)
+
+## subdata
+mydata <- subset(rawdata, helpdate=="1/2/2007" | helpdate=="2/2/2007")
+
+## graphic device
+png(filename = "plot1.png", width = 480, height = 480)
+
+## Histogramm at screen
+hist(as.numeric(mydata$Global_active_power)/1000, main="Global activ Power", xlab="Global aktiv Power (kilowatts)",col="red")
+
+## device back to screen
+dev.off()
+
+## Histogramm at screen
+hist(as.numeric(mydata$Global_active_power)/1000, main="Global activ Power", xlab="Global aktiv Power (kilowatts)",col="red")
