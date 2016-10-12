8f96fbadac6d0b6b4571df211f5d938c9d6085ce
ap-wi <ap-wi@t-online.de>
2016-10-12 18:28:45 +0200

    third commit

--- plot1_r.R
+++ plot1_r.R
@@ -5,12 +5,13 @@
 ## 10.10.2016 A.Paul
 ## --------------------------------------------------
 
+## path
 setwd("C:/Users/Paul/Desktop/coursera")
 
-## File Import
+## file import
 rawdata <- read.table("household_power_consumption.txt",header=TRUE,sep=";",dec=".")
 
-helpdate <- substring(mydata$Date,1,10)
+helpdate <- substring(rawdata$Date,1,10)
 
 ## subdata
 mydata <- subset(rawdata, helpdate=="1/2/2007" | helpdate=="2/2/2007")
@@ -18,11 +19,21 @@ mydata <- subset(rawdata, helpdate=="1/2/2007" | helpdate=="2/2/2007")
 ## graphic device
 png(filename = "plot1.png", width = 480, height = 480)
 
-## Histogramm at screen
-hist(as.numeric(mydata$Global_active_power)/1000, main="Global activ Power", xlab="Global aktiv Power (kilowatts)",col="red")
+## histogramm at screen
+hist(as.numeric(mydata$Global_active_power)/1000, 
+     main="Global Active Power",
+     xlim=c(0,6),
+     ylim=NULL,
+     xlab="Global Active Power (kilowatts)",
+     col="red")
 
 ## device back to screen
 dev.off()
 
 ## Histogramm at screen
-hist(as.numeric(mydata$Global_active_power)/1000, main="Global activ Power", xlab="Global aktiv Power (kilowatts)",col="red")
+hist(as.numeric(mydata$Global_active_power)/1000, 
+     main="Global Active Power",
+     xlim=c(0,6),
+     ylim=NULL, 
+     xlab="Global Active Power (kilowatts)",
+     col="red")
